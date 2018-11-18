46f78067c06beca87dad80edde0701fc890e0acc
Amanda Souza <amanda.minueza@gmail.com>
2018-11-18 13:49:08 -0300

    Changing namespace

--- kubernetes-install.sh
+++ kubernetes-install.sh
@@ -33,6 +33,6 @@ helm init
 
 cd 02-wordsmith-app && docker-compose build && kubectl apply -f 01-namespace.yaml
 
-kubectl apply -f 02-kube-deployment.yml && kubectl apply -f 01-nginx-ingress-waf/nginx-ingress-waf-modsecurity.yaml
+kubectl apply -f 02-kube-deployment.yml && kubectl apply -f ../01-nginx-ingress-waf/nginx-ingress-waf-modsecurity.yaml
 
 kubectl apply -f 03-ingress-wordsmith.yaml
